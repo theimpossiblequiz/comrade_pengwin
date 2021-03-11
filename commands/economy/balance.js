7df5601cfde09175a79f5827d3aa2a05005cd86d
Lazysensy <75103003+Lazysensy@users.noreply.github.com>
2021-03-11 13:20:59 +1100

    Updated Balance.js
    This might fix it idk tho

--- commands/economy/balance.js
+++ commands/economy/balance.js
@@ -10,19 +10,7 @@ module.exports = {
       const totalAmount = profileData.eddies + profileData.bank
 
       const target = message.mentions.members.first()
-      if(!target){
-         let moneyEmbed = new Discord.MessageEmbed()
-          .setColor("#006400")
-          .setTitle(`\`${message.author.username}\` BANK ACCOUNT`)
-          .setAuthor(`${message.author.username}`, message.author.displayAvatarURL())
-          .setDescription(
-             `User Economy Information: \n\n**Available Wallet:** \`$${profileData.eddies}\`\n **Available Bank Balance:** \`$${profileData.bank}\`\n **Total Amount**: \`$${totalAmount}\``
-          )
-          .setTimestamp()
-          .setFooter(`You, ${message.author.username} have, ${totalAmount} total eddies!`)
-
-          message.channel.send(moneyEmbed);
-      } else if(target) {
+      if(target) {
           const targetData = await profileModel.findOne({ userID: target.id, serverID: message.guild.id })
           if(!targetData) return message.channel.send("This user does not exist on this database")
           let MoneyEmbed = new Discord.MessageEmbed()
@@ -36,6 +24,18 @@ module.exports = {
           .setFooter(`You, ${message.author.username} have, ${totalAmount} total eddies!`)
 
           message.channel.send(MoneyEmbed);
+      } else if(!target){
+         let moneyEmbed = new Discord.MessageEmbed()
+          .setColor("#006400")
+          .setTitle(`\`${message.author.username}\` BANK ACCOUNT`)
+          .setAuthor(`${message.author.username}`, message.author.displayAvatarURL())
+          .setDescription(
+             `User Economy Information: \n\n**Available Wallet:** \`$${profileData.eddies}\`\n **Available Bank Balance:** \`$${profileData.bank}\`\n **Total Amount**: \`$${totalAmount}\``
+          )
+          .setTimestamp()
+          .setFooter(`You, ${message.author.username} have, ${totalAmount} total eddies!`)
+
+          message.channel.send(moneyEmbed);
       }
   }
 }
