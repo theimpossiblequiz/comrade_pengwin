9828809b58c0c4fb69c2474cfd401f227f4fb645
Lazysensy <75103003+Lazysensy@users.noreply.github.com>
2021-03-11 13:26:56 +1100

    Updated Balance.js
    this should fix it

--- commands/economy/balance.js
+++ commands/economy/balance.js
@@ -7,24 +7,24 @@ module.exports = {
     categories: 'economy',
     description: "Check the user balance",
     async execute(message, args, cmd, client, Discord, profileData) {
-      const totalAmount = profileData.eddies + profileData.bank
-
       const target = message.mentions.members.first()
       if(target) {
           const targetData = await profileModel.findOne({ userID: target.id, serverID: message.guild.id })
           if(!targetData) return message.channel.send("This user does not exist on this database")
+          const totalAmount = targetData.eddies + targetData.bank
           let MoneyEmbed = new Discord.MessageEmbed()
           .setColor("#006400")
-          .setTitle(`\`${message.author.username}\` BANK ACCOUNT`)
-          .setAuthor(`${message.author.username}`, message.author.displayAvatarURL())
+          .setTitle(`\`${target.username}\` BANK ACCOUNT`)
+          .setAuthor(`${target.username}`, target.displayAvatarURL())
           .setDescription(
-             `User Economy Information: \n\n**Available Wallet:** \`$${profileData.eddies}\`\n **Available Bank Balance:** \`$${profileData.bank}\`\n **Total Amount**: \`$${totalAmount}\``
+             `User Economy Information: \n\n**Available Wallet:** \`$${targetData.eddies}\`\n **Available Bank Balance:** \`$${targetData.bank}\`\n **Total Amount**: \`$${totalAmount}\``
           )
           .setTimestamp()
           .setFooter(`You, ${message.author.username} have, ${totalAmount} total eddies!`)
 
           message.channel.send(MoneyEmbed);
       } else if(!target){
+         const totalAmount = profileData.eddies + profileData.bank 
          let moneyEmbed = new Discord.MessageEmbed()
           .setColor("#006400")
           .setTitle(`\`${message.author.username}\` BANK ACCOUNT`)
